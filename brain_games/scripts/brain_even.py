89071bd97d8303c39a400b5e6823e666c70f2b42
IvanFoksha <v.foksh6@gmail.com>
2025-03-26 15:46:19 +0100

    Create brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,42 @@
+from random import randint
+from brain_games.cli import welcome_user
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def valide_inpute(prompt):
+    while True:
+        user_input = input(prompt).strip().lower()
+        if user_input in ['yes', 'no']:
+            return user_input
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+def play_even_game():
+    user_name = welcome_user()
+
+    correct_answer = 0
+    max_questions = 3
+
+    while correct_answer < max_questions:
+        number = randint(1, 100)
+        print(f'Question: {number}')
+
+        user_answer = valide_inpute('Your answer: ')
+
+        correct_answer = 'yes' if is_even(number) else 'no'
+
+        if user_answer == correct_answer:
+            print('Correct!')
+            correct_answer += 1
+        else:
+            print(
+                f"""
+                '{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.
+                Let's try again, {user_name}!
+                """
+            )
+            return
+    print(f'Congratulations, {user_name}!')
