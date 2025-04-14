80dfcb87062f1f292dbdbd8dc013e8a39a1cf091
IvanFoksha <v.foksh6@gmail.com>
2025-04-14 11:52:44 +0200

    Added engine.py for base game logic

---
+++ brain_games/engine.py
@@ -0,0 +1,29 @@
+from brain_games.cli import welcome_user
+
+
+def play_game(game_logic, description):
+    # Вынесена общая логика игры
+    user_name = welcome_user()
+    print(description)
+
+    correct_answers = 0
+    max_questions = 3
+
+    while correct_answers < max_questions:
+        question, correct_answer = game_logic()
+
+        print(f'Question: {question}')
+        user_answer = input('Your answer: ').strip()
+
+        if user_answer == correct_answer:
+            print('Correct!')
+            correct_answers += 1
+        else:
+            print(
+                f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {user_name}!")
+            return
+
+    print(f'Congratulations, {user_name}!')
