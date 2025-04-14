f64081f96779309fe2df2b49aec42b0dcadfe87c
IvanFoksha <v.foksh6@gmail.com>
2025-04-14 12:40:36 +0200

    Fix brain_calc, after update calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,16 @@
-from brain_games.games.calc import play_calc_game
-from brain_games.cli import welcome_user
+from brain_games.engine import play_game
+from brain_games.games.calc import generate_round
 
 
 def main():
-    user_name = welcome_user()
-    if play_calc_game():
-        print(f'Congratulations, {user_name}!')
+    description = 'What is the result of the expression?'
+
+    def geme_logic():
+        for index in range(3):
+            yield generate_round(index)
+
+    play_game(geme_logic, description)
+
 
 if __name__ == "__main__":
     main()
