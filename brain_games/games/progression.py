ae127c9a01612f28c7f5e35acc5e18639784e335
IvanFoksha <v.foksh6@gmail.com>
2025-04-14 13:18:53 +0200

    Added progression.py for base game logic

---
+++ brain_games/games/progression.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+def generate_progression(start, step, lenght):
+    return [start + step * i for i in range(lenght)]
+
+
+def hide_element(progression):
+    hidden_index = randint(0, len(progression) - 1)
+    hidden_value = progression[hidden_index]
+    progression[hidden_index] = '..'
+    return progression, str(hidden_value)
+
+
+def generate_round():
+    start = randint(1, 50)
+    step = randint(1, 10)
+    lenght = 10
+
+    progression = generate_progression(start, step, lenght)
+    question, correct_answer = hide_element(progression)
+
+    return ' '.join(map(str, question)), correct_answer
