3930f698b9cdba724904bf8bc9f0e4bbac4e880f
IvanFoksha <v.foksh6@gmail.com>
2025-04-14 11:08:55 +0200

    Update brain_calc for games/calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,49 +1,11 @@
-from random import randint
-
+from brain_games.games.calc import play_calc_game
 from brain_games.cli import welcome_user
 
 
-def calculate(a, b, operator):
-    if operator == '+':
-        return a + b
-    elif operator == '-':
-        return a - b
-    elif operator == '*':
-        return a * b
-    else:
-        raise ValueError(f'Неизвестный оператор: {operator}')
-
-
-def play_calc_game():
-    user_name = welcome_user()
-    print('What is the result of the expression?')
-
-    operations = ['+', '-', '*']
-
-    for operator in operations:
-        a = randint(1, 100)
-        b = randint(1, 100)
-
-        if operator == '-':
-            a, b = max(a, b), min(a, b)
-
-        correct_answer = calculate(a, b, operator)
-
-        print(f'Question: {a} {operator} {b}')
-        user_answer = input('Your answer: ').strip()
-
-        if user_answer.isdigit() and int(user_answer) == correct_answer:
-            print('Correct!')
-        else:
-            print(
-                f"'{user_answer}' is wrong answer ;(. "
-                f"Correct answer was '{correct_answer}'."
-            )
-            print(f"Let's try again, {user_name}!")
-            return
-
-    print(f'Congratulations, {user_name}!')
-
-
 def main():
-    play_calc_game()
+    user_name = welcome_user()
+    if play_calc_game():
+        print(f'Congratulations, {user_name}!')
+
+if __name__ == "__main__":
+    main()
