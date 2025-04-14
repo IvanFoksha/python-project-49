06fd600f1c3ea5f88856ff31634ee3a00b134679
IvanFoksha <v.foksh6@gmail.com>
2025-04-14 13:05:01 +0200

    Update logic brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,45 +1,16 @@
-from random import randint
-
-from brain_games.cli import welcome_user
-
-
-def prime_number(number):
-    if number < 2:
-        return False
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
-
-
-def play_game_prime_number():
-    user_name = welcome_user()
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-
-    correct_answers = 0
-    max_questions = 3
-
-    while correct_answers < max_questions:
-        number = randint(1, 100)
-
-        correct_answer = "yes" if prime_number(number) else "no"
-
-        print(f'Question: {number}')
-        user_answer = input('Your answer: ').strip().lower()
-
-        if user_answer == correct_answer:
-            print('Correct!')
-            correct_answers += 1
-        else:
-            print(
-                f"'{user_answer}' is wrong answer ;(. "
-                f"Correct answer was '{correct_answer}'."
-            )
-            print(f"Let's try again, {user_name}!")
-            return
-
-    print(f'Congratulations, {user_name}!')
+from brain_games.engine import play_game
+from brain_games.games.prime import generate_round
 
 
 def main():
-    play_game_prime_number()
+    description = 'Answer "yes" if given number is prime, otherwise answer "no".'
+
+    def game_logic():
+        for _ in range(3):
+            yield generate_round()
+
+    play_game(game_logic, description)
+
+
+if __name__ == "__main__":
+    main()
