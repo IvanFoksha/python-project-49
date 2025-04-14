aac111ab6f7defe1cc852aba4d1722419b3c2e8e
IvanFoksha <v.foksh6@gmail.com>
2025-04-14 13:18:31 +0200

    Update logic brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,51 +1,16 @@
-from random import randint
-
-from brain_games.cli import welcome_user
-
-
-def generate_progression(start, step, lenght):
-    return [start + step * i for i in range(lenght)]
-
-
-def hide_element(progression):
-    hidden_index = randint(0, len(progression) - 1)
-    hidden_value = progression[hidden_index]
-    progression[hidden_index] = '..'
-    return progression, str(hidden_value)
-
-
-def play_progression_game():
-    user_name = welcome_user()
-    print('What number is missing in the progression?')
-
-    correct_answers = 0
-    max_questions = 3
-
-    while correct_answers < max_questions:
-        start = randint(1, 50)
-        step = randint(1, 10)
-        lenght = 10
-
-        progression = generate_progression(start, step, lenght)
-
-        question, correct_answer = hide_element(progression)
-
-        print(f"Question: {' '.join(map(str, question))}")
-        user_answer = input('Your answer: ').strip()
-
-        if user_answer == correct_answer:
-            print('Correct!')
-            correct_answers += 1
-        else:
-            print(
-                f"'{user_answer}' is wrong answer ;(. "
-                f"Correct answer was '{correct_answer}'."
-            )
-            print(f"Let's try again, {user_name}!")
-            return
-
-    print(f'Congratulations, {user_name}!')
+from brain_games.engine import play_game
+from brain_games.games.progression import generate_round
 
 
 def main():
-    play_progression_game()
+    description = 'What number is missing in the progression?'
+
+    def geme_logic():
+        for _ in range(3):
+            yield generate_round()
+
+    play_game(geme_logic, description)
+
+
+if __name__ == "__main__":
+    main()
