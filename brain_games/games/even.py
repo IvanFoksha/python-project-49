d86de2c34e26aaf36c57044725d7f0972d42da1d
IvanFoksha <v.foksh6@gmail.com>
2025-04-14 11:55:49 +0200

    Update even file, after adding engine.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,14 @@
+# brain_games/games/even.py
+
+from random import randint
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def generate_round():
+    number = randint(1, 100)
+    correct_answer = 'yes' if is_even(number) else 'no'
+    question = str(number)
+    return question, correct_answer
